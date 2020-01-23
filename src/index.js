f745fb00316733337aefe97745f30ee217f83c23
matty brant <matty.brant@fmgsuite.com>
2020-01-23 17:15:52 -0600

    builds with babel and webpack, what a miracle

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom";
 
 import AASchedulingEmailButton from "./App";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <AASchedulingEmailButton
-    marketingGroupID="9"
-    assetID="1234"
-    emailTitle="My email title"
-    campaignsHomeUrl="https://3b5du.csb.app/"
-  />,
-  rootElement
-);
+function renderAASchedulingEmailButton(rootElement) {
+  ReactDOM.render(
+    <AASchedulingEmailButton
+      marketingGroupID="9"
+      assetID="1234"
+      emailTitle="My email title"
+      campaignsHomeUrl="https://3b5du.csb.app/"
+    />,
+    rootElement
+  );
+}
+
+renderAASchedulingEmailButton(document.getElementById("index"));
